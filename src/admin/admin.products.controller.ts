45e48762bf1c0aee05737c528097f2f1451cca86
PdskDev <prestadesk@gmail.com>
2022-06-29 20:54:38 +0200

    Delete product from Admin Ok

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -8,6 +8,7 @@ import {
   Redirect,
   UseInterceptors,
   UploadedFile,
+  Param,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductsService } from 'src/models/products.service';
@@ -39,5 +40,11 @@ export class AdminProductsController {
     newProduct.setPrice(body.price);
     newProduct.setImage(file.filename);
     await this.productsService.createOrUpdate(newProduct);
+  }
+
+  @Post('/:id')
+  @Redirect('/admin/products')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
   }
 }
